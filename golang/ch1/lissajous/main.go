223cf28c0ba78751d7dfd0353f0a3c1269a0f2a5
Ildarzzz1 <Nerkisil007@gmail.com>
2026-09-18 14:49:13 +0500

    gif

--- golang/ch1/lissajous/main.go
+++ golang/ch1/lissajous/main.go
@@ -1,5 +1,11 @@
-// Lissajous генерирует анимированный GIF из случайных
-// фигур Лиссажу.
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Run with "web" command-line argument for web server.
+// See page 13.
+//!+main
+
+// Lissajous generates GIF animations of random Lissajous figures.
 package main
 
 import (
@@ -7,33 +13,58 @@ import (
 	"image/color"
 	"image/gif"
 	"io"
+	"log"
 	"math"
 	"math/rand"
+	"net/http"
 	"os"
+	"time"
 )
+
+//!-main
+// Packages not needed by version in book.
+
+//!+main
 
 var palette = []color.Color{color.White, color.Black}
 
 const (
-	whiteIndex = 0 // Первый цвет палитры
-	blackIndex = 1 // Следующий цвет палитры
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
 )
 
 func main() {
+	//!-main
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
 	lissajous(os.Stdout)
 }
 
 func lissajous(out io.Writer) {
 	const (
-		cycles  = 5     // Количество полных колебаний x
-		res     = 0.001 // Угловое разрешение
-		size    = 100   // Канва изображения охватывает [-size..+size]
-		nframes = 64    // Количество кадров анимации
-		delay   = 8     // Задержка между кадрами (единица - 10мс)
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
 	)
-	freq := rand.Float64() * 3.0 // Относительная частота колебаний y
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // Разность фаз
+	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
@@ -47,5 +78,7 @@ func lissajous(out io.Writer) {
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim) // Примечание: игнорируем ошибки
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
+
+//!-main
